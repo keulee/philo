4f7f08693d7298ae704e64d1cf5a1d536de4758c
Keungeun LEE <klee8903@gmail.com>
2021-11-27 18:01:25 +0100

    updated

--- philo.c
+++ philo.c
@@ -4,10 +4,32 @@ int		main(int ac, char **av)
 {
 	(void)ac;
 	(void)av;
-	printf("start\n");
 
-	struct timeval;
+	// struct timeval;
+	// gettimeofday();
 
-	gettimeofday()
+	/* usleep test
+	printf("test usleep\n");
+	printf("text appears in 10 sec\n");
+	int i = 1;
+	while (i <= 10)
+	{
+		printf("%d sec\n", i++);
+		usleep(1000000); // 마이크로세컨드 -> 1/1000000
+	}
+	printf("HELLO\n");
+	*/
+	
+	int		t;
+	struct timeval tv;
+
+	t = gettimeofday(&tv, NULL);
+	if (t == -1)
+	{
+		perror("gettimeofday() call error");
+		return (-1);
+	}
+	printf("tv sec : tv usec = %ld : %d\n", tv.tv_sec, tv.tv_usec / 15000);
+
 	return (0);
 }
