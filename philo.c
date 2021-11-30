6c5122a5dcf532bbaf12e79cf80d47248b84170a
keulee <klee8903@gmail.com>
2021-11-30 23:10:25 +0100

    study

--- philo.c
+++ philo.c
@@ -8,28 +8,41 @@ int		main(int ac, char **av)
 	// struct timeval;
 	// gettimeofday();
 
-	/* usleep test
-	printf("test usleep\n");
-	printf("text appears in 10 sec\n");
-	int i = 1;
-	while (i <= 10)
-	{
-		printf("%d sec\n", i++);
-		usleep(1000000); // 마이크로세컨드 -> 1/1000000
-	}
-	printf("HELLO\n");
-	*/
+	// //usleep test
+	// printf("test usleep\n");
+	// printf("text appears in 10 sec\n");
+	// int i = 1;
+	// while (i <= 10)
+	// {
+	// 	printf("%d sec\n", i++);
+	// 	usleep(1000 * 1000); // 마이크로세컨드 -> 1/1000000
+	// }
+	// printf("HELLO\n");
 	
-	int		t;
-	struct timeval tv;
+	// gettimeofday test
+	// int		t;
+	// struct timeval tv;
 
-	t = gettimeofday(&tv, NULL);
-	if (t == -1)
-	{
-		perror("gettimeofday() call error");
-		return (-1);
-	}
-	printf("tv sec : tv usec = %ld : %d\n", tv.tv_sec, tv.tv_usec / 15000);
+	// t = gettimeofday(&tv, NULL);
+	// if (t == -1)
+	// {
+	// 	perror("gettimeofday() call error");
+	// 	return (-1);
+	// }
+	// printf("tv sec : tv usec = %ld : %d\n", tv.tv_sec, tv.tv_usec / 15000);
+
+	struct timeval starttime;
+	struct timeval endtime;
+	double diff_tv_sec;
+	double diff_tv_usec;
+
+	gettimeofday(&starttime, NULL);
+	usleep(1000 * 1000);
+	gettimeofday(&endtime, NULL);
+	diff_tv_sec = endtime.tv_sec - starttime.tv_sec;
+	diff_tv_usec = (endtime.tv_usec - starttime.tv_usec) / 1000;
+
+
 
 	return (0);
 }
