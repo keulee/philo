be910c6245bf617d30d439b45099f662a8dd6be4
keulee <klee8903@gmail.com>
2021-12-08 23:16:28 +0100

    updataed

--- philo.c
+++ philo.c
@@ -4,8 +4,8 @@
 void *routine()
 {
 	printf("Test from threads\n");
-	// sleep(3);
-	// printf("Ending test\n");
+	sleep(3);
+	printf("Ending test\n");
 }
 
 int		main(int ac, char **av)
@@ -13,10 +13,13 @@ int		main(int ac, char **av)
 	(void)ac;
 	(void)av;
 	pthread_t t1;
+	pthread_t t2;
 
 	/*thread create*/
 	pthread_create(&t1, NULL, &routine, NULL);  //thread init
+	pthread_create(&t2, NULL, &routine, NULL);  //thread init
 	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
 
 	// struct timeval;
 	// gettimeofday();
@@ -49,20 +52,12 @@ int		main(int ac, char **av)
 	// double diff_tv_sec;
 	// double diff_tv_usec;
 
-<<<<<<< HEAD
-	gettimeofday(&starttime, NULL);
-	usleep(1000 * 1000);
-	gettimeofday(&endtime, NULL);
-	diff_tv_sec = endtime.tv_sec - starttime.tv_sec;
-	diff_tv_usec = (endtime.tv_usec - starttime.tv_usec) / 1000;
-	
-=======
 	// gettimeofday(&starttime, NULL);
 	// usleep(1000 * 1000);
 	// gettimeofday(&endtime, NULL);
 	// diff_tv_sec = endtime.tv_sec - starttime.tv_sec;
 	// diff_tv_usec = (endtime.tv_usec - starttime.tv_usec) / 1000;
->>>>>>> d250506c37cb8534fd70149c43e3077098881a4a
+	
 
 
 
