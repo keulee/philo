7c4aa53ecc588de64101aaaffb40d926b48b3f31
Keungeun LEE <klee8903@gmail.com>
2022-01-03 22:17:04 +0100

    header on

--- philo.c
+++ philo.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keulee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/03 22:15:30 by keulee            #+#    #+#             */
+/*   Updated: 2022/01/03 22:15:35 by keulee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "includes/philo.h"
 
 int	main(int ac, char **av)
@@ -17,8 +29,7 @@ int	main(int ac, char **av)
 	monitor_thread(&info);
 	free_thread(&info);
 	if (!info.die)
-		printf("All philo miam miam at least %d times\n", \
-		info.max_eatcount);
+		printf("All philo miam miam at least %d times\n", info.max_eatcount);
 	if (info.philo)
 		free(info.philo);
 	if (info.fork)
